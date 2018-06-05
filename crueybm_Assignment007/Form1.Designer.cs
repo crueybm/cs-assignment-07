c336c358100e26737977b4ad5ce9054b6405294e
crueybm <crueybm@mail.uc.edu>
2018-06-05 04:23:33 -0400

    Add project files.

---
+++ crueybm_Assignment007/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace crueybm_Assignment007
+{
+    partial class frmAssignment07
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbBinaryString1 = new System.Windows.Forms.TextBox();
+            this.lblBinaryString01 = new System.Windows.Forms.Label();
+            this.tbBinaryString2 = new System.Windows.Forms.TextBox();
+            this.lblBinaryString2 = new System.Windows.Forms.Label();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.tbWarning = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbBinaryString1
+            // 
+            this.tbBinaryString1.Location = new System.Drawing.Point(130, 12);
+            this.tbBinaryString1.Name = "tbBinaryString1";
+            this.tbBinaryString1.Size = new System.Drawing.Size(142, 20);
+            this.tbBinaryString1.TabIndex = 0;
+            this.tbBinaryString1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbBinaryString1_KeyPress_1);
+            // 
+            // lblBinaryString01
+            // 
+            this.lblBinaryString01.AutoSize = true;
+            this.lblBinaryString01.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBinaryString01.Location = new System.Drawing.Point(12, 16);
+            this.lblBinaryString01.Name = "lblBinaryString01";
+            this.lblBinaryString01.Size = new System.Drawing.Size(112, 16);
+            this.lblBinaryString01.TabIndex = 1;
+            this.lblBinaryString01.Text = "Binary String 1:";
+            // 
+            // tbBinaryString2
+            // 
+            this.tbBinaryString2.Location = new System.Drawing.Point(131, 39);
+            this.tbBinaryString2.Name = "tbBinaryString2";
+            this.tbBinaryString2.Size = new System.Drawing.Size(141, 20);
+            this.tbBinaryString2.TabIndex = 2;
+            this.tbBinaryString2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbBinaryString2_KeyPress);
+            // 
+            // lblBinaryString2
+            // 
+            this.lblBinaryString2.AutoSize = true;
+            this.lblBinaryString2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBinaryString2.Location = new System.Drawing.Point(12, 43);
+            this.lblBinaryString2.Name = "lblBinaryString2";
+            this.lblBinaryString2.Size = new System.Drawing.Size(112, 16);
+            this.lblBinaryString2.TabIndex = 3;
+            this.lblBinaryString2.Text = "Binary String 2:";
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(74, 65);
+            this.tbResult.Name = "tbResult";
+            this.tbResult.ReadOnly = true;
+            this.tbResult.Size = new System.Drawing.Size(198, 20);
+            this.tbResult.TabIndex = 4;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(12, 69);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(56, 16);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "Result:";
+            // 
+            // tbWarning
+            // 
+            this.tbWarning.Location = new System.Drawing.Point(15, 91);
+            this.tbWarning.Name = "tbWarning";
+            this.tbWarning.ReadOnly = true;
+            this.tbWarning.Size = new System.Drawing.Size(257, 20);
+            this.tbWarning.TabIndex = 6;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(102, 117);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 7;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // frmAssignment07
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 148);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.tbWarning);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.lblBinaryString2);
+            this.Controls.Add(this.tbBinaryString2);
+            this.Controls.Add(this.lblBinaryString01);
+            this.Controls.Add(this.tbBinaryString1);
+            this.Name = "frmAssignment07";
+            this.Text = "Assignment 07";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbBinaryString1;
+        private System.Windows.Forms.Label lblBinaryString01;
+        private System.Windows.Forms.TextBox tbBinaryString2;
+        private System.Windows.Forms.Label lblBinaryString2;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox tbWarning;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ crueybm_Assignment007/Form1.cs
@@ -0,0 +1,100 @@
+﻿/*
+ * Name: Brandon Cruey 
+ * email: crueybm@mail.uc.edu
+ * Class: Contemporary Programming
+ * Date: 10/12/2017
+ * Assignment: 007
+ * Description: This program takes two arrays of binary numbers, then verifies that they only contain 0 and 1 and are the same length.
+ * The binary numbers are then converted to strings, and added using base 2.  A Windows Form is created, and reacts to the user's
+ * input to find a resulting sum.
+ * The resulting sum is then converted back into an int array, checked for length, and returned to the main.
+ * Citations: https://stackoverflow.com/questions/5074575/what-is-the-best-way-in-c-sharp-to-convert-string-into-int
+ * https://stackoverflow.com/questions/37805179/how-can-i-make-a-textbox-accepts-only-numeric-values
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace crueybm_Assignment007
+{
+    public partial class frmAssignment07 : Form
+    {
+        public frmAssignment07()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            tbResult.Clear();                                               //Clears tbResult TextBox upon button click
+            tbWarning.Clear();                                              //Clears tbWarning TextBox upon button click
+            string input1 = string.Join<char>(",", tbBinaryString1.Text);   //Declares and sets input1 to the value entered to tbBinaryString1, then converts it to a comma-separated string
+            string input2 = string.Join<char>(",", tbBinaryString2.Text);   //Declares and sets input2 to the value entered to tbBinaryString2, then converts it to a comma-separated string
+            int[] arr1 = input1.Split(',').Select(int.Parse).ToArray();     //Declares and sets arr1 to the converted value of input1, and splits it to an array
+            int[] arr2 = input2.Split(',').Select(int.Parse).ToArray();     //Declares and sets arr2 to the converted value of input2, and splits it to an array
+            int[] sum = AddBinary(arr1, arr2);                              //Calls upon the AddBinary method, using arr1 and arr2 as arguments
+            String final = string.Join("", sum);                            //Converts and joins the sum[] array into a string 
+            tbResult.Text = final;                                          //Sets the tbResult TextBox to the value of final
+        }
+
+        public int[] AddBinary(int[] op1, int[] op2)
+        {
+            int[] result;                          //Declares result array.
+            if (op1.Length != op2.Length)    //Verifies that the two binary arrays are the same length.
+            {
+                tbWarning.Text = "Binary strings are not the same length!";     //Sets tbWarning to the exception message
+                throw new Exception("Binary strings are not the same length");  //Throws the resulting exception.
+            }
+            for (int i = 0; i < op1.Length; i++)   //Begins a for loop to check array consistency.
+            {
+                if (op1[i] > 1 || op2[i] > 1)      //Verifies that the numbers in the binary arrays are no greater than 0 or 1
+                {
+                    tbWarning.Text = "Number greater than 1 detected!";             //Sets tbWarning to the exception message
+                    throw new Exception("Number greater than 1 detected");          //Throws the resulting exception.
+                }
+            }
+
+            string tmp1 = string.Join("", op1);         //Converts the int op1 array into a string.
+            string tmp2 = string.Join("", op2);         //Converts the int op2 array into a string.
+            int tmp11 = Convert.ToInt32(tmp1, 2);       //Converts the tmp1 string into a base 2 integer.
+            int tmp22 = Convert.ToInt32(tmp2, 2);       //Converts the tmp2 string into a base 2 integer.
+            string res = Convert.ToString(tmp11 + tmp22, 2).PadLeft(op1.Length, '0');
+            //Adds the tmp1 and tmp2 binary integers, converts the sum into a string, and then pads the left of the string with 0.
+            string res1 = string.Join<char>(",", res);  //Inserts commas into the res string for the next step.
+            result = res1.Split(',').Select(int.Parse).ToArray();
+            //Changes the binary sum into an int array.
+            if (result.Length != op1.Length)            //Checks the result array against the original array to verify length.
+            {
+                tbWarning.Text = "Result length does not match Operand Length!";        //Sets tbWarning to the exception message
+                throw new Exception("Result Length does not match Operand Length");     //Throws the resulting exception
+            } else
+            {
+                tbWarning.Text = "Operation successful!";       //Sets tbWarning to a success message
+            }
+            return result;   //Returns the resulting array to the main
+        }
+
+        private void tbBinaryString1_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void tbBinaryString2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
